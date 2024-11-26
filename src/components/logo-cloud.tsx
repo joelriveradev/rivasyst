993ffb48c7c6b82867bce56feb29f390394a80d6
joelriveradev <joelriveradev@gmail.com>
2024-11-26 14:54:40 -0600

    chore: remove watson logo

--- src/components/logo-cloud.tsx
+++ src/components/logo-cloud.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export function LogoCloud() {
   return (
     <div className='w-full my-20'>
-      <p className='mb-14 md:mb-4 mx-auto lg:text-xl text-center text-stone-300'>
+      <p className='mb-14 md:mb-10 mx-auto lg:text-xl text-center text-stone-300'>
         I've worked with some of the largest brands in the world
       </p>
 
@@ -33,15 +33,6 @@ export function LogoCloud() {
           priority
           alt='CNN logo'
           className='scale-75 lg:scale-100 md:-ml-4 lg:-ml-3'
-        />
-
-        <Image
-          src='/watson.svg'
-          width={100}
-          height={92.5}
-          priority
-          alt='IBM Watson logo'
-          className='md:ml-8 lg:-ml-3'
         />
       </div>
     </div>
